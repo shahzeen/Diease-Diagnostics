5a1fad625eff7fe52192d5b7a3baf89109528650
Tanmoy Chaudhury <tanmchau@in.ibm.com>
2018-08-08 21:24:32 +0530

    update

--- public/pages/about/controllers/abouttabCntrl.js
+++ public/pages/about/controllers/abouttabCntrl.js
@@ -1,5 +1,5 @@
 'use strict'; 
-billapp.controller('abouttabController', ['$scope','$uibModalInstance', function ($scope, $uibModalInstance) {
+billapp.controller('abouttabController', ['$scope','$uibModalInstance', function ($scope, $uibModalInstance){
 	
 	console.log('On abouttabController');
 	
